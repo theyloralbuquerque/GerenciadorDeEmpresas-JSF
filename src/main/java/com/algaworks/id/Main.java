78a0dc387e5a110762ef6eb36455a1e70b87a691
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-07-24 22:25:37 -0300

    Exemplo de diferentes aplicações da injeção de dependências

--- src/main/java/com/algaworks/id/Main.java
+++ src/main/java/com/algaworks/id/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		Pedidos pedidos = new Pedidos();
 		
 		RelatorioService relatorioService = new RelatorioService();
-		relatorioService.setPedidos(pedidos);
+		//relatorioService.setPedidos(pedidos);
 	}
 
 }
--- src/main/java/com/algaworks/id/Pedidos.java
+++ src/main/java/com/algaworks/id/Pedidos.java
@@ -2,7 +2,13 @@ package com.algaworks.id;
 
 import java.math.BigDecimal;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
 public class Pedidos {
+	
+	@Inject
+	private EntityManager entityManager;
 
 	public BigDecimal totalPedidosMesAtual() {
 		return new BigDecimal("100");
