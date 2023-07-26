7bd405b96706b3d15a852df9f019c95a84aaafe8
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-07-26 20:54:52 -0300

    Criando o managed bean

--- src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -5,19 +5,28 @@ import java.io.Serializable;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import com.algaworks.erp.model.Empresa;
+import com.algaworks.erp.model.TipoEmpresa;
+
 @Named // @Named deixa a classe acessível a qualquer uma das páginas xhtml do projeto.
 @ViewScoped // Define o escopo de um bean gerenciado para ser associado ao ciclo de vida da visualização (view) de uma página JSF.
 public class GestaoEmpresasBean implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
-	private static Integer NUMERO = 0;
+	private Empresa empresa = new Empresa();
 	
-	public GestaoEmpresasBean() {
-		NUMERO++;
+	public void salvar() {
+		System.out.println("Razão social: " + empresa.getRazaoSocial()
+				+ " - Nome fantasia : " + empresa.getNomeFantasia()
+				+ " - Tipo: " + empresa.getTipo());
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
 	}
 	
-	public Integer getNumero() {
-		return NUMERO;
+	public TipoEmpresa[] getTiposEmpresa() {
+		return TipoEmpresa.values();
 	}
 }
